9320a7d6609ae7676087f5eb7953192af8d086fb
优孟 <397317382@qq.com>
2025-09-05 15:20:49 +0900

    1

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ while true; do
     password=$(bashio::config 'password')
     haToken=$(bashio::config 'long_live_token')
 
-    USERNAME="$username" PASSWORD="$password" HA_TOKEN="$haToken" data/build/enecle-linux-arm64
+    USERNAME="$username" PASSWORD="$password" HA_TOKEN="$haToken" /enecle-linux-arm64
     sleep 1800
 done
